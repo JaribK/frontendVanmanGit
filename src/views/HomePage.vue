f2b117ea75cef4d7e7740cbc6be39b3508805ce1
JaribK <mewn7788@gmail.com>
2024-04-24 11:26:24 +0700

    Update

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -614,13 +614,13 @@ import moment from 'moment'
             },
 
             async logout(){
-                // await axios.post(host + 'api/logout/',{},
-                //     {
-                //         headers: {
-                //             'Authorization': `Token ${localStorage.getItem('token')}`
-                //         }
-                //     }
-                // ).then(() => {
+                await axios.post(host + 'api/logout/',{},
+                    {
+                        headers: {
+                            'Authorization': `Token ${localStorage.getItem('token')}`
+                        }
+                    }
+                ).then(() => {
                     localStorage.removeItem('user');
                     localStorage.removeItem('token');
                     swal.fire({
@@ -630,9 +630,9 @@ import moment from 'moment'
                         confirmButtonText: 'OK'
                     })
                     this.$router.push('/login');
-                // }).catch((err) => {
-                //     console.log(err)
-                // })
+                }).catch((err) => {
+                    console.log(err)
+                })
             },
 
             getuser(){
