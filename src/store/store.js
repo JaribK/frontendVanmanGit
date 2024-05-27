aaf67c6eeecb5cd19c208750e86dd58aa0698081
JaribK <mewn7788@gmail.com>
2024-05-27 13:06:18 +0700

    Update

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import Vuex from 'vuex';
 
 const store = new Vuex.Store({
     state: {
-        host:'http://127.0.0.1:8000/'
+        host:'https://backendvanman-production.up.railway.app'
     },
     mutations: {
     },
